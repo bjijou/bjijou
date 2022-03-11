327a80232d9f1cd09d78f073d032e2f47c331f48
Aissa <bjijou1980@gmail.com>
2022-03-11 11:45:33 +0100

    Update test1.php
    Cambio 2 después del

--- test1.php
+++ test1.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <link rel="icon" href="https://www.cebi.com/sites/default/files/favicon.ico">
-    <title> LOGIN EDI PORTAL </title>
+    <title>WEB1 </title>
 
     <link rel="stylesheet" href="css/style.css" type="text/css" />
     <link rel="stylesheet" href="css/img.css" type="text/css" />
@@ -24,7 +24,7 @@
     <div id="container">
         <form action="includes/verification.php" method="POST" autocomplete="off">
             <p class="aligncenter"> <img src="img/cebi_logo.png" alt="CEBI"> </p>
-            <h1>EDI PORTAL</h1>
+            <h1>PORTAL 1</h1>
             <label><b>Username(email)</b></label>
             <input type="email" name="username" placeholder="your cebi email" id="username" required>
             <label><b>Password</b></label>
@@ -54,22 +54,7 @@
 
             <p>New user? <a href="mailto:aissa.el-hammouti@cebi.com"> Contact portal admin </a></p>
         </form>
-
-        <div class="u-flag">
-            <p class="aligncenter"> Access to WIKI </p>
-            <a href="https://lu11edi-wa01/dokuwiki/doku.php?id=wikicebi:wikicebilux" target="_blank" style="padding-left: 120px "><img src="img/lu.png" alt="Cebi luxembourg" width="50" height="50"/></a>
-            <a href="https://lu11edi-wa01/dokuwiki/doku.php?id=wikicebi:wikicebipl" target="_blank" alt="Cebi poland" style="padding-left: 50px "><img src="img/pl.png"  width="50" height="50"/></a>
-            <!-- <a href="http://" target="_blank" style="padding-right: 0px"><img src="img/mx.png" alt="Cebi mx" width="50" height="50"></a> -->
-        </div>
-    </div>
-    <script type="text/javascript" language="javascript" class="init">
-        $(document).ready(function() {
-            $('input[type=submit]').click(function() {
-                $(this).attr('disabled', 'disabled');
-                $(this).parents('form').submit();
-            });
-        });
-    </script>
+ 
 </body>
 
 </html>
